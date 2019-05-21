54da3912263942fe64cf19d04a8b500261ac8f26
Trent Moss <trentmoss05@gmail.com>
2019-05-21 01:44:33 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -21,7 +21,7 @@ export default class ChromeBoisDomain extends Component {
       <canvas 
         onMouseMove={this.handleMouseMove}
         onClick={() => {toggleCycling()}}
-        
+        onKeyDown={this.onKeyDown}
         width='900'
         height='600'
         tabIndex="0">
