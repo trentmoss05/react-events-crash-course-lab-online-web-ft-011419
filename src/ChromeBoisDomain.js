4c6a07247654b5d8f39c8361c4bd25a2941528f4
Trent Moss <trentmoss05@gmail.com>
2019-05-21 01:43:08 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -20,6 +20,7 @@ export default class ChromeBoisDomain extends Component {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+        onClick={
         width='900'
         height='600'
         tabIndex="0">
